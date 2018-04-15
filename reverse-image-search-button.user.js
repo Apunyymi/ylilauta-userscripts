1442039310c7855a9aadfdade40ef31f3e6ea13a
AnonyymiHerrasmies <38400241+AnonyymiHerrasmies@users.noreply.github.com>
2018-04-15 20:57:58 +0300

    MutationObserver changed to newRepliesListener

--- reverse-image-search-button.user.js
+++ reverse-image-search-button.user.js
@@ -1,51 +1,47 @@
 // ==UserScript==
-// @name Ylilauta reverse image search button
+// @name Ylilauta.fi: Käänteinen kuvahaku
 // @namespace Violentmonkey Scripts
 // @match *://ylilauta.org/*
 // @grant none
-// @version 0.4
+// @version 0.5
 // @locale en
-// @description Adds a button for reverse image search
+// @description Lisää nappulan käänteiselle kuvahaulle
 // ==/UserScript==
 
-const addSearchButtons = () => {
-  const posts = Array.from(document.querySelectorAll('div.op_post, div.answer'))
+function addSearchButtons() {
+  Array.from(document.querySelectorAll('div.op_post, div.answer'))
     .map(div => {
-      const post = Array.from(div.childNodes).find(n => n.className === 'post')
+      const post = Array.from(div.childNodes).find(n => n.className === 'post');
       const filecontainer = Array.from(post.childNodes).find(
         n => n.className && n.className.indexOf('filecontainer thumbnail file') !== -1
-      )
-      if (filecontainer === undefined) return
-      const expandlink = Array.from(filecontainer.childNodes).find(n => n.className === 'expandlink')
-      if (expandlink === undefined) return
-      const href = expandlink.href
-      if (href === undefined) return
-      const postinfo = Array.from(div.childNodes).find(n => n.className === 'postinfo')
-      const messageoptions = Array.from(postinfo.childNodes).find(n => n.className === 'messageoptions')
-      const magnifier = Array.from(messageoptions.childNodes).find(n => n.className === 'icon-magnifier')
+      );
+      if (filecontainer === undefined) return;
+      const expandlink = Array.from(filecontainer.childNodes).find(n => n.className === 'expandlink');
+      if (expandlink === undefined) return;
+      const href = expandlink.href;
+      if (href === undefined) return;
+      const postinfo = Array.from(div.childNodes).find(n => n.className === 'postinfo');
+      const messageoptions = Array.from(postinfo.childNodes).find(n => n.className === 'messageoptions');
+      const magnifier = Array.from(messageoptions.childNodes).find(n => n.className === 'icon-magnifier');
       if (magnifier === undefined) {
-        const link = document.createElement('a')
-        link.className = 'icon-magnifier'
-        link.href = 'https://images.google.com/searchbyimage?image_url=' + href
-        link.title = 'Käänteinen kuvahaku'
-        link.target = '_blank'
-        messageoptions.insertBefore(link, messageoptions.children[1])
+        const link = document.createElement('a');
+        link.className = 'icon-magnifier';
+        link.href = 'https://images.google.com/searchbyimage?image_url=' + href;
+        link.title = 'Käänteinen kuvahaku';
+        link.target = '_blank';
+        messageoptions.insertBefore(link, messageoptions.children[1]);
       }
     })
 }
 
-addSearchButtons()
+function newRepliesListener(callback) {
+  updateQuotes = () => {
+    callback();
 
-const targetDiv = document.querySelector('div.answers')
-const config = { childList: true }
+    return updateQuotes;
+  };
+}
 
-const observer = new MutationObserver(
-  (mutationsList) => {
-    if (Array.from(mutationsList).filter(
-      (mutation) => mutation.type === 'childList').length > 0) {
-      addSearchButtons()
-    }
-  }
-)
+addSearchButtons();
 
-observer.observe(targetDiv, config)
+newRepliesListener(() => addSearchButtons());
