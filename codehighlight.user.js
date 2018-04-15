55777338f29a97a05ccc2bfd0261b3746ac0908d
ohjelmoija <pyr.yrouvila@gmail.com>
2018-04-15 18:48:58 +0300

    update code highlight to highlight new messages asynchronously

--- codehighlight.user.js
+++ codehighlight.user.js
@@ -1,18 +1,32 @@
 // ==UserScript==
-// @name Ylilauta.fi: Koodinväritys
+// @name Ylilauta.fi: Code Highlighter
 // @namespace Violentmonkey Scripts
 // @match *://ylilauta.org/*
 // @require https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
 // @resource highlightCSS https://gitcdn.xyz/repo/isagalaev/highlight.js/cf4b46e5b7acfe2626a07914e1d0d4ef269aed4a/src/styles/darcula.css
 // @grant GM_addStyle
 // @grant GM_getResourceText
-// @version 0.1
-// @description Värittää [code]koodinpätkät[/code]
+// @version 0.4
+// @description Highlight code blocks with Highlight.js
 // ==/UserScript==
+
+function newRepliesListener(callback) {
+  const observer = new MutationObserver(callback);
+  
+  observer.observe(
+    $('.answers')[0], 
+    { childList: true }
+  );
+}
+
+function highlightCode() {
+  $('pre.pre').each((i, block) => {
+    hljs.highlightBlock(block);
+  });
+}
 
 const highlightCSS = GM_getResourceText('highlightCSS');
 GM_addStyle(highlightCSS);
 
-$('pre.pre').each((i, block) => {
-  hljs.highlightBlock(block);
-});
+highlightCode();
+newRepliesListener(() => highlightCode());
