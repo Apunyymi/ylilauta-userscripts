536f52dd01b083940bdf000a0dac50a62335f500
ohjelmoija <38399731+ohjelmoija@users.noreply.github.com>
2018-04-15 23:06:17 +0300

    lisää kaikkien viestien lainaaminen

---
+++ quoteallfromip.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name Ylilauta.fi: Lainaa kaikki viestit IP:ltä
+// @namespace Violentmonkey Scripts
+// @match *://ylilauta.org/*
+// @version 0.2
+// @description Lainaa kaikki viestit samalta IP:ltä (vaatii Kultatilin)
+// ==/UserScript==
+
+function quoteAllIps(event) {
+  const clickedMessage = $(event.target).parent();
+  clickedMessage.children('.postuid.ip').click();
+
+  const clickedIp = clickedMessage.children('.postuid.ip').text();
+
+  const quotes = $('.postuid.ip')
+    .map(function() {
+      if ($(this).text() === clickedIp) {
+        const parent = $(this).parent();
+        const postId = parent
+          .parents('.answer')
+          .first()
+          .attr('data-msgid');
+
+        if (postId) {
+          return '>>' + postId + '\n';
+        }
+      }
+    })
+    .get()
+    .join('');
+
+  $('#msg').insertAtCaret(quotes);
+}
+
+const quoteAllButton = $('<span />', {
+  text: '>> Quote ips',
+  class: 'postuid',
+  on: {
+    click: quoteAllIps
+  }
+});
+
+$('.postuid.id').after(quoteAllButton);
