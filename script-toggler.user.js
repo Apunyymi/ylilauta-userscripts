d8e3aa7d84dcef1064917a5387e47fca64758d75
AnonyymiHerrasmies <38400241+AnonyymiHerrasmies@users.noreply.github.com>
2018-04-17 20:01:15 +0300

    Script toggler

--- script-toggler.user.js
+++ script-toggler.user.js
@@ -2,31 +2,59 @@
 // @name Ylilauta: Script toggler
 // @namespace Violentmonkey Scripts
 // @match *://ylilauta.org/*
-// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/toggler/reverse-image-search-button.user.js
-// @grant none
+// @require https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
+// @require https://gitcdn.xyz/repo/Stuk/jszip/9fb481ac2a294f9c894226ea2992919d9d6a70aa/dist/jszip.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/autoscroll-button.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/codehighlight.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/downloadall-button.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/ippostcounter.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/lastownpost-button.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/namefag-hider.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/quoteallfromip-button.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/reverse-image-search-button.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/show-most-answered.user.js
+// @require https://github.com/AnonyymiHerrasmies/ylilauta-userscripts/raw/dea590d9b69cc9fa185e2ab19ec5171200308099/tagpost-hider.user.js
+// @resource highlightCSS https://gitcdn.xyz/repo/isagalaev/highlight.js/cf4b46e5b7acfe2626a07914e1d0d4ef269aed4a/src/styles/darcula.css
+// @grant GM_addStyle
+// @grant GM_getResourceText
 // ==/UserScript==
 
-
 function isToggled(name) {
-  return localStorage.getItem(name) === "true";
+  const item = localStorage.getItem(name);
+  if (item === undefined || item === "undefined") {
+    localStorage.setItem(name, false);
+    return false;
+  }
+  return item !== "false";
 }
 
 function getInput(name) {
-  const input = document.createElement('input')
-  input.type = "checkbox"
-  input.checked = isToggled(name)
+  const input = document.createElement('input');
+  input.type = "checkbox";
+  input.checked = isToggled(name);
   input.onchange = function() {
-    localStorage.setItem(name, this.checked)
+    localStorage.setItem(name, this.checked);
   }
-  return input
+  return input;
 }
 
 if (window.location.href.indexOf("/preferences?site") > -1) {
   const preferencesDiv = $('#site');
   
-  preferencesDiv.prepend(getInput("reverseImageSearchStorage"), " Käänteinen kuvahaku <br/>");
+  const scriptDiv = document.createElement('div');
+  $(scriptDiv).append("<h3>Skriptit</h3>");
   
-  const header = document.createElement('h3');
-  header.innerText = "Skriptit"
-  preferencesDiv.prepend(header)
+  $(scriptDiv).append(getInput("autoscrollStorage"), " Autoscroll-nappula <br/>");
+  $(scriptDiv).append(getInput("codeHighlighterStorage"), " [code]Koodi[/code]-blokkien väritys <br/>");
+  $(scriptDiv).append(getInput("downloadAllStorage"), " Lataa kaikki -nappula <br/>");
+  $(scriptDiv).append(getInput("ipPostCounterStorage"), " Näytä käyttäjän postausten määrä (vain kultatilillä) <br/>");
+  $(scriptDiv).append(getInput("lastOwnPostStorage"), " Last own post -nappula <br/>");
+  $(scriptDiv).append(getInput("ipPostCounterStorage"), " Näytä käyttäjän postausten määrä (vain kultatilillä) <br/>");
+  $(scriptDiv).append(getInput("namefagHiderStorage"), " Piilota nimipostaukset <br/>");
+  $(scriptDiv).append(getInput("quoteAllFromIpStorage"), " Vastaa kaikkiin käyttäjän postauksiin -nappula <br/>");
+  $(scriptDiv).append(getInput("reverseImageSearchStorage"), " Käänteinen kuvahaku <br/>");
+  $(scriptDiv).append(getInput("showMostAnsweredStorage"), " Näytä vastatuimmat -nappula <br/>");
+  $(scriptDiv).append(getInput("tagpostHiderStorage"), " Piilota tagipostaukset <br/>");
+  
+  preferencesDiv.prepend(scriptDiv)
 }
