5a78a3c1e3c253ca672dd0af222ef6966884cb3b
Apu Apustaja <38398354+Apunyymi@users.noreply.github.com>
2018-04-28 11:11:37 +0300

    Loppu se toimimattoman paskan tunkkaaminen tuotantoon

--- script-toggler/spam-hider.user.js
+++ script-toggler/spam-hider.user.js
@@ -22,7 +22,7 @@
   function hideThreads() {
     const threadMap = {};
 
-    allPosts.each(function () {
+    $('.thread').each(function () {
       const text = this.innerText;
 
       if (threadMap[text] === undefined) {
