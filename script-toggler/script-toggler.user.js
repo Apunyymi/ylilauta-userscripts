7eaa616942aaf869b07f4f50e249fa0a599850d2
Apu Apustaja <38398354+Apunyymi@users.noreply.github.com>
2018-05-12 23:19:08 +0300

    Versionumeron päivitys

--- script-toggler/script-toggler.user.js
+++ script-toggler/script-toggler.user.js
@@ -2,7 +2,7 @@
 // @name Ylilauta: Script toggler
 // @namespace Violentmonkey Scripts
 // @match *://ylilauta.org/*
-// @version 1.3.9
+// @version 1.3.10
 // @require https://static.ylilauta.org/js/jquery-3.3.1.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js
 // @require https://gitcdn.xyz/repo/Stuk/jszip/9fb481ac2a294f9c894226ea2992919d9d6a70aa/dist/jszip.js
