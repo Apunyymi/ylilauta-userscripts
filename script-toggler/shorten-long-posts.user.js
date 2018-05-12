7d352e1f588c24dcf57bc1a52d0bedb3a014a9ef
Apu Apustaja <38398354+Apunyymi@users.noreply.github.com>
2018-05-12 23:16:53 +0300

    Nyt postauslyhennin toimimaan myös teemoilla ja customboardeilla

--- script-toggler/shorten-long-posts.user.js
+++ script-toggler/shorten-long-posts.user.js
@@ -13,55 +13,63 @@ runSafely(() => {
 		const thresold = JSON.parse(localStorage.getItem('shortenLongPostsThresold') || '10');
 		const shownRows = JSON.parse(localStorage.getItem('shortenLongPostsShownRows') || '8');
 
-		GM_addStyle(`
+		let postSubjectTest = $('#right .postsubject:first-of-type');
+
+		// No need to run if we don't match
+		if (postSubjectTest.length) {
+			let buttonBackground = getComputedStyle($('#right .postsubject:first-of-type')[0]).backgroundColor;
+			let shortenedBorder = getComputedStyle($('.linkbutton:first-of-type')[0]).borderBottomColor;
+
+			GM_addStyle(`
 .shortened-post {
 	height: ${shownRows}em;
-	border-bottom: 1px solid #d9bfb7;
+	border-bottom: 1px solid ${shortenedBorder};
 	overflow: hidden;
 	resize: vertical;
 }
 .shortenbutton {
-	background-color: #d9bfb7;
+	background-color: ${buttonBackground};
 	margin: 5px;
 }
-`);
+	`);
 
-		function onButtonClick (eve) {
-			let el = $(eve.target);
-			el.prev().removeClass('shortened-post').css({height: '', maxHeight: ''});
-			el.remove();
-		}
-
-		function shorten (el) {
-			let root = $(el);
-			let post = root.find('.post');
-			let kkontent = post.find('.postcontent');
-
-			let styles = getComputedStyle(kkontent[0]);
-			let fontSize = parseFloat(styles.fontSize);
-
-			if ((kkontent[0].clientHeight / fontSize) > thresold) {
-				kkontent.css('max-height', styles.height)
-				kkontent.addClass('shortened-post');
-
-				post.append(
-					$('<button class="linkbutton shortenbutton">Tätä postausta on lyhennetty, paina tästä niin näet koko viestin.</button>')
-						.click(onButtonClick)
-				);
+			function onButtonClick (eve) {
+				let el = $(eve.target);
+				el.prev().removeClass('shortened-post').css({height: '', maxHeight: ''});
+				el.remove();
 			}
-		}
 
-		let query = '';
-		if ($('#right.thread').length) {
-			query = '#right .answer';
-		} else if ($('#right.board').length) {
-			query = '#right .thread';
-		}
+			function shorten (el) {
+				let root = $(el);
+				let post = root.find('.post');
+				let kkontent = post.find('.postcontent');
 
-		if (query) {
-			$(query).each((i, el) => {
-				shorten(el);
-			});
+				let styles = getComputedStyle(kkontent[0]);
+				let fontSize = parseFloat(styles.fontSize);
+
+				if ((kkontent[0].clientHeight / fontSize) > thresold) {
+					kkontent.css('max-height', styles.height)
+					kkontent.addClass('shortened-post');
+
+					post.append(
+						$('<button class="linkbutton shortenbutton">Tätä postausta on lyhennetty, paina tästä niin näet koko viestin.</button>')
+							.click(onButtonClick)
+					);
+				}
+			}
+
+			let query = '';
+			if ($('#right.thread').length) {
+				query = '#right .answer';
+			} else if ($('#right.board,#right.customboard').length) {
+				query = '#right .thread';
+			}
+
+			if (query) {
+				$(query).each((i, el) => {
+					shorten(el);
+				});
+			}
 		}
 	}
 });
